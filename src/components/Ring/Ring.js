7e05bf5fb95047c881a481935ba684d3bed02f97
x3malmusic <x3malmusic@gmail.com>
2021-08-19 17:37:12 +0300

    add OrbitRing component

---
+++ src/components/Ring/Ring.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Ring } from "@react-three/drei";
+
+const OrbitRing = ({ distanceFromAxis }) => {
+  const [distance] = distanceFromAxis
+
+  return(
+    <Ring position={[0, 0, 0]} args={[distance, distance + 0.1, 320]} rotation={[1.57, 0, 0]}>
+      <meshBasicMaterial attach="material" side={2} />
+    </Ring>
+  )
+}
+
+export default OrbitRing;
