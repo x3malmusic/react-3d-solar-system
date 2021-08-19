c4808a0e43a3d754459502e257bf3c7e3f4e7973
x3malmusic <x3malmusic@gmail.com>
2021-08-19 17:36:53 +0300

    new planets added

--- src/constants/solarSystem.js
+++ src/constants/solarSystem.js
@@ -1,15 +1,40 @@
 import EarthImg from '../assets/textures/earth.jpg'
 import SunImg from '../assets/textures/sun.jpg'
+import MercuryImg from "../assets/textures/mercury.jpg"
+import VenusImg from "../assets/textures/venus.jpg"
+import MarsImg from "../assets/textures/mars.jpg"
+import JupiterImg from "../assets/textures/jupiter.jpg"
+import SaturnImg from "../assets/textures/saturn.jpg"
+import UranusImg from "../assets/textures/uranus.jpg"
+import NeptuneImg from "../assets/textures/neptune.jpg"
+import PlutoImg from "../assets/textures/pluto.jpg"
+
 import MoonImg from '../assets/textures/moon.jpg'
+
+export const sun = {
+  name: "Sun",
+  size: 20,
+  texture: SunImg,
+  rotationSpeed: 0.00004,
+  position: [0, 0, 0],
+}
 
 export const solarSystem = [
   {
-    name: "Sun",
-    size: 20,
-    texture: SunImg,
-    rotationSpeed: 0.00004,
-    position: [0, 0, 0],
-    moveSpeed: 0,
+    name: "Mercury",
+    size: 0.5,
+    texture: MercuryImg,
+    rotationSpeed: 0.0005,
+    position: [22, 0, 0],
+    moveSpeed: 0.001,
+  },
+  {
+    name: "Venus",
+    size: 1,
+    texture: VenusImg,
+    rotationSpeed: 0.0002,
+    position: [30, 0, 0],
+    moveSpeed: 0.0005,
   },
   {
     name: "Earth",
@@ -23,8 +48,56 @@ export const solarSystem = [
       size: 0.3,
       texture: MoonImg,
       rotationSpeed: 0.0002,
-      position: [5.5, 0, 0],
+      position: [2.5, 0, 0],
       moveSpeed: 0.0001,
     }]
+  },
+  {
+    name: "Mars",
+    size: 0.8,
+    texture: MarsImg,
+    rotationSpeed: 0.0002,
+    position: [55, 0, 0],
+    moveSpeed: 0.00009,
+  },
+  {
+    name: "Jupiter",
+    size: 10,
+    texture: JupiterImg,
+    rotationSpeed: 0.0002,
+    position: [75, 0, 0],
+    moveSpeed: 0.00002,
+  },
+  {
+    name: "Saturn",
+    size: 9,
+    texture: SaturnImg,
+    rotationSpeed: 0.0002,
+    position: [100, 0, 0],
+    moveSpeed: 0.000008,
+  },
+  {
+    name: "Uranus",
+    size: 5,
+    texture: UranusImg,
+    rotationSpeed: 0.0002,
+    position: [120, 0, 0],
+    moveSpeed: 0.000001,
+  },
+  {
+    name: "Neptune",
+    size: 5,
+    texture: NeptuneImg,
+    rotationSpeed: 0.0002,
+    position: [140, 0, 0],
+    moveSpeed: 0.0000005,
+  },
+  {
+    name: "Pluto",
+    size: 0.3,
+    texture: PlutoImg,
+    rotationSpeed: 0.0002,
+    position: [155, 0, 0],
+    moveSpeed: 0.00000001,
   },
 ]
